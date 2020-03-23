b73c3bf211662b86aaf0fa374f1555d7d519c33b
Sansiel <deviler.san@yandex.ru>
2020-03-23 09:42:08 +0400

    Добавьте файлы проекта.

---
+++ Miakd_1_dist/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miakd_1_dist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
